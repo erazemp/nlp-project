c75476be0dfa013fbfbcecb0a58af4b258a4ad8f
asmaljcelj <aljaz.smaljcelj7@gmail.com>
2022-04-16 21:10:32 +0200

    Corpus creation:
    - added simple bag of words to determine sentence context

---
+++ src/corpus_construction/context_extraction/bow/bow.py
@@ -0,0 +1,93 @@
+import json
+import scipy.spatial
+
+
+def read_corpus(filename):
+    # read JSON file created in preprocess step
+    f = open(filename)
+    data = json.load(f)
+    f.close()
+    return data
+
+
+def construct_bow(pairs, window):
+    bow_builder = BowBuilder(window)
+    # iterate through all the sentences to get context vectors
+    for pair in pairs:
+        word = pair["word"]
+        bow_builder.construct_neighbouring_vector(pair["lemma_sentence1"], word, pair["lemma_word_index1"])
+        bow_builder.construct_neighbouring_vector(pair["lemma_sentence2"], word, pair["lemma_word_index2"])
+    return bow_builder
+
+
+def fill_vectors(bow_builder, pairs, cosine_distance_threshold):
+    for pair in pairs:
+        word = pair["word"]
+        context_vector1 = bow_builder.count_occurrences(pair["lemma_sentence1"], word, pair["lemma_word_index1"])
+        context_vector2 = bow_builder.count_occurrences(pair["lemma_sentence2"], word, pair["lemma_word_index2"])
+        distance = calculate_cosine_distance(context_vector1, context_vector2)
+        if distance > cosine_distance_threshold:
+            pair["same_context"] = True
+    return pairs
+
+
+def save_json_file(pairs, json_filename):
+    json_str = json.dumps([p for p in pairs])
+    # save to JSON file
+    with open(json_filename, "w") as outfile:
+        outfile.write(json_str)
+
+
+def calculate_cosine_distance(vector1, vector2):
+    v1 = []
+    v2 = []
+    for word in vector1.keys():
+        v1.append(vector1[word])
+        v2.append(vector2[word])
+    return 1 - scipy.spatial.distance.cosine(v1, v2)
+
+
+class BowBuilder:
+    def __init__(self, n):
+        self.lemma_neighboring_words = {}
+        self.n = n
+
+    def construct_neighbouring_vector(self, lemma_sentence, lemma, index):
+        if lemma not in self.lemma_neighboring_words:
+            self.lemma_neighboring_words[lemma] = {}
+        neighbouring_words = self.get_neighboring_words(lemma_sentence, index)
+        for neighbouring_word in neighbouring_words:
+            # don't include lemma as it is always present
+            if neighbouring_word == lemma:
+                continue
+            if neighbouring_word not in self.lemma_neighboring_words[lemma]:
+                self.lemma_neighboring_words[lemma][neighbouring_word] = 0
+
+    def count_occurrences(self, lemma_sentence, lemma, index):
+        neighbouring_words = self.get_neighboring_words(lemma_sentence, index)
+        context_vector = self.lemma_neighboring_words[lemma].copy()
+        for neighbouring_word in neighbouring_words:
+            # don't include lemma as it is always present
+            if neighbouring_word == lemma:
+                continue
+            context_vector[neighbouring_word] = context_vector[neighbouring_word] + 1
+        return context_vector
+
+    def get_neighboring_words(self, lemma_sentence, index):
+        words = lemma_sentence.split(" ")
+        start = index - self.n
+        if start < 0:
+            start = 0
+        end = index + self.n + 1
+        if end > len(words):
+            end = len(words)
+        return words[start:end]
+
+
+if __name__ == '__main__':
+    corpus_file = '../../preprocess/corpus.json'
+    window_size = 2
+    corpus_entries = read_corpus(corpus_file)
+    bow = construct_bow(corpus_entries, window_size)
+    updated_pairs = fill_vectors(bow, corpus_entries, 0.7)
+    save_json_file(updated_pairs, 'new_corpus.json')
--- src/corpus_construction/preprocess/preprocess.py
+++ src/corpus_construction/preprocess/preprocess.py
@@ -19,7 +19,7 @@ def extract_sentences_from_corpus(directory, homonyms):
     for filename in os.listdir(directory):
         # TEMP for quick assessment
         i = i + 1
-        if i > 50:
+        if i > 5000:
             break
         # end TEMP
         print('parsing file ', filename)
@@ -39,18 +39,18 @@ def extract_homonym_sentences(filename, homonyms, homonym_sentences):
         contains_homonym_lemma = False
         start_index = -1
         end_index = -1
-        lemma_start_index = -1
-        lemma_end_index = -1
+        lemma_index = -1
+        word_index = -1
         for word_xml in sentence_parsed:
             # if entry is not a word, skip it
             if not word_xml.text:
                 continue
             # check if current word has a lemma
             if "lemma" in word_xml.attrib:
+                word_index = word_index + 1
                 # check if word lemma is a homonym
                 if word_xml.attrib["lemma"] in homonyms:
-                    lemma_start_index = len(lemma_sentence)
-                    lemma_end_index = lemma_start_index + len(word_xml.attrib["lemma"]) + 1
+                    lemma_index = word_index
                     contains_homonym_lemma = True
                     lemma = word_xml.attrib["lemma"]
                     # check if entry for this lemma already exists, if not, create one
@@ -69,8 +69,7 @@ def extract_homonym_sentences(filename, homonyms, homonym_sentences):
             sentence = sentence + word_xml.text
         # if in this sentence we have a homonym lemma, add it to our list
         if contains_homonym_lemma:
-            sentence_entry = SentenceEntry(sentence, lemma_sentence, start_index, end_index, lemma_start_index,
-                                           lemma_end_index)
+            sentence_entry = SentenceEntry(sentence, lemma_sentence, start_index, end_index, lemma_index)
             homonym_sentences[lemma].append(sentence_entry)
     return homonym_sentences
 
@@ -105,31 +104,28 @@ class OutputEntry:
         self.lemma_sentence2 = sentence2_object.lemma_sentence
         self.sentence2 = sentence2_object.sentence
         self.start1 = sentence1_object.start
-        self.lemma_start1 = sentence1_object.lemma_start
+        self.lemma_word_index1 = sentence1_object.lemma_word_index
         self.end1 = sentence1_object.end
-        self.lemma_end1 = sentence1_object.lemma_end
         self.start2 = sentence2_object.start
-        self.lemma_start2 = sentence2_object.lemma_start
+        self.lemma_word_index2 = sentence2_object.lemma_word_index
         self.end2 = sentence2_object.end
-        self.lemma_end2 = sentence2_object.lemma_end
         self.same_context = False
 
 
 class SentenceEntry:
-    def __init__(self, sentence, lemma_sentence, start, end, lemma_start, lemma_end):
+    def __init__(self, sentence, lemma_sentence, start, end, lemma_index):
         self.sentence = sentence
         self.lemma_sentence = lemma_sentence
         self.start = start
         self.end = end
-        self.lemma_start = lemma_start
-        self.lemma_end = lemma_end
+        self.lemma_word_index = lemma_index
 
 
 if __name__ == '__main__':
     gigafida_dirname = '../../../Gigafida_corpus'
     homonyms_filename = 'homonyms.txt'
     corpus_filename = 'corpus.json'
-    word_limit = 2
+    word_limit = 10000000
     homonym = read_homonyms_file(homonyms_filename)
     homonyms_sentences = extract_sentences_from_corpus(gigafida_dirname, homonym)
     construct_output_temp(homonyms_sentences, corpus_filename, word_limit)
